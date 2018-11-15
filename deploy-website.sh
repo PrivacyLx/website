c8b9ee8cb469efbf10dca40186f038dcbec01788
anadahz <andz@torproject.org>
2018-11-15 12:48:33 +0000

    Fix deploy to restart container if running, increase verbosity

--- deploy-website.sh
+++ deploy-website.sh
@@ -2,18 +2,26 @@
 privacylx_website="/home/privacylx/website/public"
 caddyfile="/home/privacylx/website/Caddyfile"
 cert_path="/home/privacylx/website/.caddy"
+container_name="privacylx-website"
 
 # pull from the remote repo
-git pull
+git pull --verbose
 
 # build the static website with hugo
 hugo
 
-docker run -d --restart=always \
-    -e "ACME_AGREE=true" \
-    -v ${privacylx_website}:/srv \
-    -v ${caddyfile}:/etc/Caddyfile \
-    -v ${cert_path}:/root/privacylx/.caddy \
-    -p 80:80 \
-    -p 443:443 \
-    abiosoft/caddy:0.11.0-no-stats
+# check if the container running and create if it's not running
+if [ -n "$(docker ps -qf name="${container_name}")" ]; then
+        printf "Restarting container...\n"
+        docker restart "${container_name}"
+else
+        docker run -d --restart=always \
+            -e "ACME_AGREE=true" \
+            -v ${privacylx_website}:/srv \
+            -v ${caddyfile}:/etc/Caddyfile \
+            -v ${cert_path}:/root/privacylx/.caddy \
+            -p 80:80 \
+            -p 443:443 \
+            --name "${container_name}" \
+            abiosoft/caddy:0.11.0-no-stats
+fi
